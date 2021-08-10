e9fd5bb86b03275a175428e51a948266a6d50812
Patrick Petersen <88366778+Hannibal83dk@users.noreply.github.com>
2021-08-10 14:00:37 +0200

    Add files via upload

---
+++ R/exceltoolsV3.R
@@ -0,0 +1,96 @@
+
+#' @import readxl
+#' @import openxlsx
+
+library(readxl)
+library(openxlsx)
+
+chooseSheet <- function(sheets){
+  value = 0
+  cat("Choose a sheet to add concentrations \n")
+  for (i in sheets) {
+    cat(paste(which(sheets == i), i, "\n"))
+  }
+
+
+
+  while (value == 0) {
+    value <- as.numeric(readline(prompt = "Select: "))
+  }
+
+  return(value)
+}
+
+
+#' load specific sheets from a chosen excel file
+#'
+#' Let you chose a file from a popup window, then lists the sheets to choose from
+#'
+#'
+#' @return A tibble containing the data from the chosen sheet
+#' @export
+#'
+readsheet <- function(){
+  cat("Choose a file to add concentrations")
+  file <- file.choose()
+
+  sheets <- excel_sheets(file)
+
+  sheet <- chooseSheet(sheets)
+
+  ##  need only pick first sheet if there only is one, then no selecteion from user
+
+  datasheet <- read_excel(file, sheet = sheet)
+
+  return(datasheet)
+}
+
+
+
+
+##  appendtoexcellfile <- function(data, file){}
+
+#' Create an excel file and save your tables as sheets in that document
+#'
+#' As of yet there is no way of giving the sheets other names.
+#' The sheets in the resulting document will therefore inherit the variable names given in this function.
+#' e.g. table1 will give rise to a sheet in the excel document called table1.
+#'
+#' @param title A filename for the excel file to be created
+#' @param ... A number of dataframes to be used as sheets for the new excel file
+#' @param overwrite A logical toggle to choose whether to overwrite a potential existing file
+#' @return A tibble containing the data from the chosen sheet
+#' @export
+#'
+createexcelfile <- function(title, ..., overwrite = FALSE){
+
+  wb <- createWorkbook()
+
+  sheets <- list(...)
+  sheetnames <- as.list(substitute(list(...)))
+  sheetnames <- unlist(sheetnames[2: length(sheetnames)])
+
+  for(i in 1:length(list(...))){
+    addWorksheet(wb, sheetnames[[i]])
+    writeData(wb, sheetnames[[i]], sheets[[i]])
+  }
+
+  saveWorkbook(wb, paste(title, ".xlsx", sep=""), overwrite = overwrite)
+
+  cat(paste("The file ", title, ".xlsx", " has been saved at ", as.character(getwd()), sep = ""))
+}
+
+
+
+#testsheet1 <- readsheet()
+#testsheet2 <- readsheet()
+#testsheet3 <- readsheet()
+
+#createexcelfile("excelfiletest3", testsheet1)
+
+
+
+#winDialog(type = c("ok", "okcancel", "yesno", "yesnocancel"), message)
+
+#winDialog(type = c("1"), "message")
+#winMenuAdd("Testit")
